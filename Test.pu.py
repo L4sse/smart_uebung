990b6dccf9b9bc6f332261313a3f7d7a83eb31d6
L4sse <lasshoferharald@gmail.com>
2018-07-03 20:21:53 +0200

    initial commit

---
+++ Test.pu.py
@@ -0,0 +1,14 @@
+x = 3
+y = 4
+operation = raw_input("Bitte geben Sie wie folg ein, + - / ")
+
+if operation == "+":
+    print x + y
+elif operation == "-":
+    print x - y
+elif operation == "/":
+    print x / y
+else == "*":
+    print x * y
+
+
