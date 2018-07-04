16ffc92fa88a3a7377a50d5f8345571c9c08227a
L4sse <40795709+L4sse@users.noreply.github.com>
2018-07-04 23:08:33 +0200

    Add files via upload

---
+++ Hausuebung 1.1.py
@@ -0,0 +1,13 @@
+Zufallszahl = raw_input("Wahle eine Zahl zwischen 1 und 5 aus")
+if Zufallszahl == "1":
+    print "Erhoehe deine Zahl um 3"
+elif Zufallszahl == "2":
+    print "Erhoehe deine Zahl um 3"
+elif Zufallszahl == "3":
+    print "Das ist eine gute Entscheidung"
+elif Zufallszahl == "4":
+    print "Verringere deine Zahl um 2"
+elif Zufallszahl == "5":
+    print "Verringere deine Zahl um 2"
+else:
+    print "Inkorrekte Eingabe"
---
+++ Hausuebung 1.py
@@ -0,0 +1,5 @@
+secret = raw_input("Errate die magische Zahl zwischen 1 und 9")
+if secret == "5":
+    print "Herzliche Gratulation"
+else:
+    print " Leider nein"
