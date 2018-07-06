1d8fc084b356442196657a52b0c7dd78e4b56718
L4sse <40795709+L4sse@users.noreply.github.com>
2018-07-06 17:39:21 +0200

    Add files via upload

---
+++ Hausuebung 1.2.py
@@ -0,0 +1,9 @@
+zufallsfarbe =raw_input("Waehle zwischen den Farben rot gelb und gruen aus")
+if zufallsfarbe == "rot":
+    print "Halt. Versuche eine andere Farbe"
+elif zufallsfarbe == "gelb":
+    print "Stopp, versuche eine andere Farbe"
+elif zufallsfarbe == "gruen":
+    print "Das ist die korrekte Zufallsfarbe"
+else:
+    print "Falsche Eingabe"
